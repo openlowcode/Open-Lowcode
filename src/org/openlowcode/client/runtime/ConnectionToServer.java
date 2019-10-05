690086ddfada72a6e1c4a0b584e0d612b774f00d
Open Lowcode SAS <welcome@openlowcode.com>
2019-10-05 12:38:51 +0200

    Check-in ConnectionToServer

---
+++ src/org/openlowcode/client/runtime/ConnectionToServer.java
@@ -0,0 +1,196 @@
+/********************************************************************************
+ * Copyright (c) 2019 [Open Lowcode SAS](https://openlowcode.com/)
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0 .
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+package org.openlowcode.client.runtime;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.util.logging.Logger;
+
+import org.openlowcode.tools.messages.MessageBufferedWriter;
+import org.openlowcode.tools.messages.MessageReader;
+import org.openlowcode.tools.messages.MessageSimpleReader;
+import org.openlowcode.tools.messages.MessageWriter;
+
+/**
+ * Wraps the socket connection to the Open Lowcode server, and includes relaunch
+ * mechanisms if a network connection is broken
+ * 
+ * @author <a href="https://openlowcode.com/" rel="nofollow">Open Lowcode
+ *         SAS</a>
+ *
+ */
+public class ConnectionToServer {
+
+	private Socket clientsocket;
+	private MessageSimpleReader reader;
+	private MessageBufferedWriter writer;
+	private String server = null;
+	private int port = -1;
+
+	/**
+	 * Should include all actions to send a full command to the server. The
+	 * ConnectionToServer may relaunch it several times in case of interruption of
+	 * transmission.
+	 * 
+	 * @author <a href="https://openlowcode.com/">Open Lowcode SAS</a>
+	 *
+	 */
+	@FunctionalInterface
+	public interface WriterToServer {
+		void apply(MessageWriter t) throws IOException;
+	}
+
+	private static final Logger logger = Logger.getLogger(ConnectionToServer.class.getName());
+
+	/**
+	 * Sends the message to the server, restarting the connection if necessary
+	 * 
+	 * @param writertoserver function to send the message to the server
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public void sendMessage(WriterToServer writertoserver) throws UnknownHostException, IOException {
+		int index = 0;
+		boolean sent = false;
+		while ((index < 2) && (!sent)) {
+			try {
+				writertoserver.apply(writer);
+
+				sent = true;
+			} catch (IOException e) {
+				logger.warning("Client disconnected");
+				clientsocket = new Socket(server, new Integer(port).intValue());
+				reader = new MessageSimpleReader(new BufferedReader(
+						new InputStreamReader(clientsocket.getInputStream(), Charset.forName("UTF-8")), 9090));
+				writer = new MessageBufferedWriter(
+						new BufferedWriter(
+								new OutputStreamWriter(clientsocket.getOutputStream(), Charset.forName("UTF-8"))),
+						true);
+				index++;
+			}
+		}
+	}
+
+	/**
+	 * @return the server (after it has been initiated by the method
+	 *         connectToAddressAndGetApplication )
+	 */
+	public String getServer() {
+		return server;
+	}
+
+	/**
+	 * @return the port (after it has been initiated by the method
+	 *         connectToAddressAndGetApplication )
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * creates the connection to server. It then needs to be initiated by the method
+	 * connectToAddressAndGetApplication
+	 */
+	public ConnectionToServer() {
+	}
+
+	/**
+	 * @param address the local address (action reference)
+	 * @return the full address (with server URL)
+	 * @throws IOException
+	 */
+	public String connectToAddressAndGetApplication(String address) throws IOException {
+		int localport = 8080;
+		int columnindex = address.indexOf(':');
+		int slashindex = address.indexOf('/');
+		int serverend = slashindex; // by default, no port
+		if ((columnindex >= 0) && (columnindex < slashindex)) {
+			// there is a column before first slash : port is indicated
+			serverend = columnindex;
+			String portstring = address.substring(columnindex + 1, slashindex);
+			localport = new Integer(portstring).intValue();
+		}
+		String localserver = address.substring(0, serverend);
+		String application = address.substring(slashindex + 1);
+		if ((clientsocket == null) || ((localserver.compareTo(server) != 0) || (localport != port))) {
+			if (clientsocket != null) {
+				if (clientsocket.isConnected())
+					clientsocket.close();
+				if (reader != null)
+					reader.close();
+			}
+			clientsocket = new Socket(localserver, new Integer(localport).intValue());
+			InputStreamReader streamreader = new InputStreamReader(clientsocket.getInputStream(),
+					Charset.forName("UTF-8"));
+			logger.fine("Input stream reader encoding " + streamreader.getEncoding());
+			BufferedReader bufferedreader = new BufferedReader(streamreader, 9090);
+
+			reader = new MessageSimpleReader(bufferedreader);
+			OutputStreamWriter socketoutputstream = new OutputStreamWriter(clientsocket.getOutputStream(),
+					Charset.forName("UTF-8"));
+			logger.fine("OutputStream reader encoding" + socketoutputstream.getEncoding());
+			BufferedWriter bufferedwriter = new BufferedWriter(socketoutputstream);
+			writer = new MessageBufferedWriter(bufferedwriter, true);
+
+			server = localserver;
+			port = localport;
+		}
+		return application;
+	}
+
+	/**
+	 * @return the reader of this connection
+	 */
+	public MessageReader getReader() {
+		return reader;
+	}
+
+	/**
+	 * @param address the local address of an action
+	 * @return the full address, including the server and port if necessary.
+	 */
+	public String completeAddress(String address) {
+		StringBuffer fulladdress = new StringBuffer();
+		fulladdress.append(server);
+		if (port != 8080) {
+			fulladdress.append(':');
+			fulladdress.append(port);
+		}
+		fulladdress.append('/');
+		fulladdress.append(address);
+		return fulladdress.toString();
+	}
+
+	/**
+	 * cancels and closes the connections
+	 * 
+	 * @throws IOException
+	 */
+	public void stopConnection() throws IOException {
+		// dereferencing the socket first, whether the close happens well or not. If
+		// there
+		// is an exception in the close() socket method, anyways, socket is dead
+		Socket sockettoclose = this.clientsocket;
+		this.clientsocket = null;
+		if (sockettoclose != null)
+			sockettoclose.close();
+		if (reader != null)
+			reader.close();
+		if (writer != null)
+			writer.close();
+	}
+}
