ae04b35c9dfdb995a0f148d2830211708a3d606a
Open Lowcode SAS <welcome@openlowcode.com>
2019-12-21 20:37:25 +0100

    Adding SDefaultPath interface

---
+++ src/org/openlowcode/server/graphic/SDefaultPath.java
@@ -0,0 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2019 [Open Lowcode SAS](https://openlowcode.com/)
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0 .
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+package org.openlowcode.server.graphic;
+
+/**
+ * An interface for all  page nodes that have a path
+ * 
+ * @author <a href="https://openlowcode.com/" rel="nofollow">Open Lowcode
+ *         SAS</a>
+ */
+
+public interface SDefaultPath {
+	/**
+	 * @return the path for this element
+	 */
+	public String getPathName();
+}
