edf5d344d086454117ff1a750b5b883a5cfe37d0
Open Lowcode SAS <welcome@openlowcode.com>
2019-12-27 12:13:30 +0100

    Another step in the great check-in

---
+++ src/org/openlowcode/server/security/ActionObjectPersonalSecurityManager.java
@@ -0,0 +1,200 @@
+package org.openlowcode.server.security;
+
+/********************************************************************************
+ * Copyright (c) 2019 [Open Lowcode SAS](https://openlowcode.com/)
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0 .
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+import java.util.HashMap;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+import org.openlowcode.module.system.data.Appuser;
+import org.openlowcode.server.action.SActionData;
+import org.openlowcode.server.data.DataObject;
+import org.openlowcode.server.data.DataObjectDefinition;
+import org.openlowcode.server.data.properties.DataObjectId;
+import org.openlowcode.server.data.properties.LinkobjectInterface;
+
+import org.openlowcode.server.data.properties.UniqueidentifiedInterface;
+import org.openlowcode.server.data.storage.AndQueryCondition;
+import org.openlowcode.server.data.storage.JoinQueryCondition;
+
+import org.openlowcode.server.data.storage.QueryConditionNever;
+import org.openlowcode.server.data.storage.QueryFilter;
+import org.openlowcode.server.data.storage.QueryOperatorEqual;
+import org.openlowcode.server.data.storage.SimpleQueryCondition;
+import org.openlowcode.server.data.storage.StoredFieldSchema;
+import org.openlowcode.server.data.storage.TableAlias;
+import org.openlowcode.server.runtime.OLcServer;
+
+/**
+ * A personal security mannager uses a link from the specific object to an
+ * application user, and gives him a personal privilege.
+ * 
+ * @author <a href="https://openlowcode.com/" rel="nofollow">Open Lowcode
+ *         SAS</a>
+ *
+ * @param <E>
+ */
+public class ActionObjectPersonalSecurityManager<E extends DataObject<E> & UniqueidentifiedInterface<E>, F extends DataObject<F> & LinkobjectInterface<F, E, Appuser>>
+		extends ActionObjectSecurityManager<E> {
+
+	private static Logger logger = Logger.getLogger(ActionObjectPersonalSecurityManager.class.getName());
+
+	private DataObjectDefinition<F> securitylinkdefinition;
+
+	private Function<DataObjectId<Appuser>, F[]> getterfromappuserid;
+	private BiFunction<DataObjectId<Appuser>, DataObjectId<E>, F[]> getterfromappuseridandobject;
+	private StoredFieldSchema<String> maintableid;
+	private StoredFieldSchema<String> securitylinkleftid;
+	private StoredFieldSchema<String> securitylinkrightid;
+	private BiFunction<SActionData, SecurityBuffer, E[]> inputactiondataextractor;
+
+	/**
+	 * Creates a personal security manager
+	 * 
+	 * @param securitylinkdefinition       definition of the security link object
+	 * @param getterfromappuserid          gets the list of security links from the
+	 *                                     provided user
+	 * @param getterfromappuseridandobject gets the list of security links for the
+	 *                                     provided user and object
+	 * @param inputactiondataextractor     the extractor to get the input objbects
+	 *                                     from the action
+	 * @param maintableid                  stored field of the main table id (used
+	 *                                     for queries)
+	 * @param securitylinkleftid           stored field of the link left id (used
+	 *                                     for queries)
+	 * @param securitylinkrightid          stored field of the link right id (used
+	 *                                     for queries)
+	 */
+	public ActionObjectPersonalSecurityManager(DataObjectDefinition<F> securitylinkdefinition,
+			Function<DataObjectId<Appuser>, F[]> getterfromappuserid,
+			BiFunction<DataObjectId<Appuser>, DataObjectId<E>, F[]> getterfromappuseridandobject,
+			BiFunction<SActionData, SecurityBuffer, E[]> inputactiondataextractor,
+			StoredFieldSchema<String> maintableid, StoredFieldSchema<String> securitylinkleftid,
+			StoredFieldSchema<String> securitylinkrightid) {
+		this.securitylinkdefinition = securitylinkdefinition;
+		this.getterfromappuserid = getterfromappuserid;
+		this.getterfromappuseridandobject = getterfromappuseridandobject;
+		this.inputactiondataextractor = inputactiondataextractor;
+		this.securitylinkleftid = securitylinkleftid;
+		this.securitylinkrightid = securitylinkrightid;
+		this.maintableid = maintableid;
+	}
+
+	@Override
+	public boolean isObjectAuthorized(E object) {
+		return this.isAuthorizedForCurrentUser(null, object);
+	}
+
+	@Override
+	public E[] getInputObject(SActionData input, SecurityBuffer buffer) {
+
+		if (inputactiondataextractor != null)
+			return inputactiondataextractor.apply(input, buffer);
+		return null;
+
+	}
+
+	@Override
+	public boolean isAuthorizedForCurrentUser(String context, E object) {
+
+		DataObjectId<Appuser> currentuserid = OLcServer.getServer().getCurrentUserId();
+		F[] authorizations = this.getterfromappuseridandobject.apply(currentuserid, object.getId());
+		if (authorizations != null)
+			if (authorizations.length > 0)
+				return true;
+		return false;
+
+	}
+
+	@Override
+	public boolean isMaybeAuthorized() {
+
+		DataObjectId<Appuser> currentuserid = OLcServer.getServer().getCurrentUserId();
+		F[] authorizations = getterfromappuserid.apply(currentuserid);
+		if (authorizations != null)
+			if (authorizations.length > 0)
+				return true;
+		return false;
+
+	}
+
+	@Override
+	public boolean queryObjectData() {
+		if (inputactiondataextractor == null)
+			return false;
+		return true;
+	}
+
+	@Override
+	public boolean filterObjectData() {
+		if (inputactiondataextractor != null)
+			return false;
+		return true;
+
+	}
+
+	@Override
+	public void freezeUnauthorizedObjects(DataObject<?>[] dataarray, SecurityBuffer buffer) {
+		DataObjectId<Appuser> currentuserid = OLcServer.getServer().getCurrentUserId();
+		F[] authorizations = getterfromappuserid.apply(currentuserid);
+		HashMap<String, F> linksperobjectid = new HashMap<String, F>();
+		if (authorizations != null)
+			for (int i = 0; i < authorizations.length; i++)
+				linksperobjectid.put(authorizations[i].getLfid().getId(), authorizations[i]);
+		for (int h = 0; h < dataarray.length; h++) {
+			try {
+				@SuppressWarnings("unchecked")
+				E object = (E) dataarray[h];
+				if (linksperobjectid.get(object.getId().getId()) != null) {
+					object.setUnfrozen();
+				} else {
+					object.setFrozen();
+				}
+			} catch (ClassCastException e) {
+				logger.warning("Exception in security manager " + e.getMessage());
+				for (int i = 0; i < e.getStackTrace().length; i++) {
+					StackTraceElement stacktrace = e.getStackTrace()[i];
+					logger.warning("   + " + stacktrace.toString());
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public Function<TableAlias, QueryFilter> getOutputFilterCondition() {
+		return new Function<TableAlias, QueryFilter>() {
+
+			@Override
+			public QueryFilter apply(TableAlias t) {
+				TableAlias personallink = new TableAlias(securitylinkdefinition.getTableschema(),
+						t.getName() + "_" + securitylinkdefinition.getName());
+				JoinQueryCondition<String> joincondition = new JoinQueryCondition<String>(t, maintableid, personallink,
+						securitylinkleftid, new QueryOperatorEqual<String>());
+				try {
+					DataObjectId<Appuser> currentuserid = OLcServer.getServer().getCurrentUserId();
+					SimpleQueryCondition<String> filterlinkbyuser = new SimpleQueryCondition<String>(personallink,
+							securitylinkrightid, new QueryOperatorEqual<String>(), currentuserid.getId());
+					return QueryFilter.get(new AndQueryCondition(joincondition, filterlinkbyuser), personallink);
+				} catch (RuntimeException e) {
+					logger.severe("Exception while generating Filter Condition " + e.getMessage());
+					for (int i = 0; i < e.getStackTrace().length; i++)
+						logger.severe("    - " + e.getStackTrace()[i]);
+					return QueryFilter.get(new QueryConditionNever());
+				}
+			}
+
+		};
+
+	}
+
+}
---
+++ src/org/openlowcode/server/security/ActionObjectSecurityManager.java
@@ -0,0 +1,91 @@
+package org.openlowcode.server.security;
+
+/********************************************************************************
+ * Copyright (c) 2019 [Open Lowcode SAS](https://openlowcode.com/)
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0 .
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+import java.util.logging.Logger;
+
+import org.openlowcode.server.action.SActionData;
+import org.openlowcode.server.data.DataObject;
+
+/**
+ * a common parent class for all security managers that requires data from the
+ * object
+ * 
+ * @author <a href="https://openlowcode.com/" rel="nofollow">Open Lowcode
+ *         SAS</a>
+ *
+ * @param <E> class of the data object
+ */
+public abstract class ActionObjectSecurityManager<E extends DataObject<E>> extends ActionSecurityManager {
+	private static Logger logger = Logger.getLogger(ActionObjectSecurityManager.class.getName());
+
+	/**
+	 * function to be implemented by the specific security manager, telling if an
+	 * object is valid or not
+	 * 
+	 * @param object the object
+	 * @return true if authorized, false if not
+	 */
+	public abstract boolean isObjectAuthorized(E object);
+
+	/**
+	 * @param input  input action data
+	 * @param buffer security buffer (should be used as much as possibl)e
+	 * @return the list of objects that have to be tested as input of the action
+	 */
+	public abstract E[] getInputObject(SActionData input, SecurityBuffer buffer);
+
+	/**
+	 * this method allows to check if an object is valid or not depending on the
+	 * context
+	 * 
+	 * @param context any context useful for the specific logic
+	 * @param object  the object
+	 * @return
+	 */
+	public abstract boolean isAuthorizedForCurrentUser(String context, E object);
+
+	/**
+	 * this method should return true if there is a possibility that the user has
+	 * the privilege for some objects. This is typically for queries where users
+	 * will be authorized to see some (but not all) of the objects. Then, the query
+	 * filter mechanism should be used
+	 * 
+	 * @return true if function is maybe authorized for the user (should use query
+	 *         filter to filter data)
+	 */
+	public abstract boolean isMaybeAuthorized();
+
+	@Override
+	public boolean isAuthorizedForCurrentUser(String context, SActionData input, SecurityBuffer buffer) {
+
+		E[] objects = this.getInputObject(input, buffer);
+		// as a rule, if the function is called with no object, returns OK if the
+		// function may be OK at some time
+		if (objects == null)
+			return isMaybeAuthorized();
+		if (objects.length == 0)
+			return isMaybeAuthorized();
+		for (int h = 0; h < objects.length; h++) {
+			E object = objects[h];
+			logger.info(" --------------------- Start Evaluation " + context + " -------------------- ");
+			boolean valid = this.isObjectAuthorized(object);
+
+			if (!valid) {
+				logger.info("object KO : " + object.dropToString());
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+}
