8e2fb8d74ea0ed720f8e75ad51e1abc3b5e12bfb
Open Lowcode SAS <welcome@openlowcode.com>
2020-02-10 06:50:29 +0100

    Another step in the great check-in

--- src/org/openlowcode/OLcVersionGenerator.java
+++ src/org/openlowcode/OLcVersionGenerator.java
@@ -26,8 +26,8 @@ import org.openlowcode.tools.file.SourceGenerator;
  *
  */
 public class OLcVersionGenerator {
-	public final static String version = "0.9.1";
-	public final static String clientversion = "0.9.1";
+	public final static String version = "1.0.0";
+	public final static String clientversion = "1.0.0";
 	public final static boolean stable = true;
 
 	public static void main(String[] args) {
