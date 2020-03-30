33eee8ee770cfb40db7fbfca00ea9349e23dfa98
Open Lowcode SAS <welcome@openlowcode.com>
2020-03-30 08:13:55 +0200

    Correcting warning

--- src/org/openlowcode/server/data/ChoiceDataObjectFieldFlatFileLoaderColumn.java
+++ src/org/openlowcode/server/data/ChoiceDataObjectFieldFlatFileLoaderColumn.java
@@ -13,18 +13,8 @@ package org.openlowcode.server.data;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
-import org.apache.poi.hssf.usermodel.DVConstraint;
-import org.apache.poi.hssf.usermodel.HSSFDataValidation;
+
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataValidation;
-import org.apache.poi.ss.usermodel.DataValidationConstraint;
-import org.apache.poi.ss.usermodel.DataValidationHelper;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.util.CellRangeAddressList;
-import org.apache.poi.xssf.usermodel.XSSFDataValidation;
-import org.apache.poi.xssf.usermodel.XSSFDataValidationConstraint;
-import org.apache.poi.xssf.usermodel.XSSFDataValidationHelper;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.openlowcode.server.data.loader.FlatFileLoader;
 import org.openlowcode.server.data.loader.FlatFileLoaderColumn;
 import org.openlowcode.server.data.loader.PostUpdateProcessingStore;
