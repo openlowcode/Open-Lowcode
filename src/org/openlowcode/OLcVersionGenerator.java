b8288893384c5972e7bc96ae4985ed46cb6c29ac
Open Lowcode SAS <welcome@openlowcode.com>
2020-05-25 12:10:52 +0200

    Version 2.0 opening

--- src/org/openlowcode/OLcVersionGenerator.java
+++ src/org/openlowcode/OLcVersionGenerator.java
@@ -26,9 +26,9 @@ import org.openlowcode.tools.file.SourceGenerator;
  *
  */
 public class OLcVersionGenerator {
-	public final static String version = "1.7.5";
-	public final static String clientversion = "1.7.5";
-	public final static boolean stable = true;
+	public final static String version = "2.0.0";
+	public final static String clientversion = "2.0.0";
+	public final static boolean stable = false;
 
 	public static void main(String[] args) {
 		try {
