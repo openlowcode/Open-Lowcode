5be8a969b5d97ca0fb0ca06ac0e31aa718ed1caf
Open Lowcode SAS <welcome@openlowcode.com>
2020-05-25 19:46:03 +0200

    Another step in implementation of Hasid

---
+++ src/org/openlowcode/server/data/properties/HasidInterface.java
@@ -0,0 +1,27 @@
+/********************************************************************************
+ * Copyright (c) 2020 [Open Lowcode SAS](https://openlowcode.com/)
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0 .
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+package org.openlowcode.server.data.properties;
+
+import org.openlowcode.server.data.DataObject;
+
+/**
+ * @author <a href="https://openlowcode.com/" rel="nofollow">Open Lowcode
+ *         SAS</a>
+ *
+ * @param <E> parent data object
+ * @since 2.0
+ */
+public interface HasidInterface<E extends DataObject<E>> {
+	/**
+	 * @return the unique id of the object
+	 */
+	public DataObjectId<E> getId();
+}
