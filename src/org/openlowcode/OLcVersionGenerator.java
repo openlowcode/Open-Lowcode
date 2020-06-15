748c40e7a150cf43d80631d904b8ee24a12dce4b
Open Lowcode SAS <welcome@openlowcode.com>
2020-06-15 22:13:22 +0200

    Opening 1.9.3

--- src/org/openlowcode/OLcVersionGenerator.java
+++ src/org/openlowcode/OLcVersionGenerator.java
@@ -26,9 +26,9 @@ import org.openlowcode.tools.file.SourceGenerator;
  *
  */
 public class OLcVersionGenerator {
-	public final static String version = "1.9.2";
-	public final static String clientversion = "1.9.2";
-	public final static boolean stable = true;
+	public final static String version = "1.9.3";
+	public final static String clientversion = "1.9.3";
+	public final static boolean stable = false;
 
 	public static void main(String[] args) {
 		try {
