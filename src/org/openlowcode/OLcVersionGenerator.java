e9c696ef54ab21ff749161fb3d726ed3945aa62a
Open Lowcode SAS <welcome@openlowcode.com>
2021-02-17 12:52:03 +0100

    Releasing 1.15.1

--- src/org/openlowcode/OLcVersionGenerator.java
+++ src/org/openlowcode/OLcVersionGenerator.java
@@ -28,9 +28,9 @@ import org.openlowcode.design.generation.SourceGenerator;
  */
 public class OLcVersionGenerator {
 
-	public final static String version = "1.15";
-	public final static String clientversion = "1.15";
-	public final static boolean stable = false;
+	public final static String version = "1.15.1";
+	public final static String clientversion = "1.15.1";
+	public final static boolean stable = true;
 
 
 	public static void main(String[] args) {
